67f7330cf6dc2895497323521d2a99b392bb3fb4
Jefferson Guanzing <jefferson.b.guanzing@gmail.com>
2023-08-12 00:59:27 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Hello There')
+
+const menu = document.querySelector('.menu')
+const close = document.querySelector('.close')
+const nav = document.querySelector('nav')
+
+menu.addEventListener('click', () => {
+    nav.classList.add('open-nav')
+})
+
+close.addEventListener('click', ()=> {
+    nav.classList.remove('open-nav')
+})
